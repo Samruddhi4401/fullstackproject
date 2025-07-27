11f7bad9b09bc7c144132b0156dbb14454b2e395
parshav42 <singhrohan98192@gmail.com>
2025-07-28 02:07:53 +0530

    ok

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://127.0.0.1:5500",
-  "https://your-netlify-site.netlify.app", // ✅ Replace with your actual Netlify frontend URL
+  "https://fullstackproject-bh7s.onrender.com", // ✅ Replace with your actual Netlify frontend URL
 ];
 
 app.use(cors({
